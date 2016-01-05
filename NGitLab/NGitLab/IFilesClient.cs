bb7e10305c09aba61d91aa3e1f887c7e016342fb
Erik J. Olson <erik.j.olson@hitchcock.org>
2016-01-05 09:42:11 -0500

    Added functionality to get commited file from repo

--- NGitLab/NGitLab/IFilesClient.cs
+++ NGitLab/NGitLab/IFilesClient.cs
@@ -4,6 +4,7 @@ namespace NGitLab
 {
     public interface IFilesClient
     {
+        FileData Get(FileGet file);
         void Create(FileUpsert file);
         void Update(FileUpsert file);
         void Delete(FileDelete file);
---
+++ NGitLab/NGitLab/Models/FileGet.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace NGitLab.Models
+{
+    [DataContract]
+    public class FileGet
+    {
+        [Required]
+        [DataMember(Name="file_path")]
+        public string Path; 
+        
+        [Required]
+        [DataMember(Name="ref")]
+        public string Ref;
+    }
+}
